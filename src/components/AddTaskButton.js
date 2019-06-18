c70a549ecf05878434a0307deb12a73e927c71fb
MikeMaven <mikemaven@gmail.com>
2019-06-18 15:56:45 -0400

    added more app page design

---
+++ src/components/AddTaskButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTaskButton = props => {
+  return(
+    <div id="add-task-button">
+      + CREATE TASK
+    </div>
+  )
+}
+
+export default AddTaskButton;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBar = props => {
   return(
-    <div>
+    <div id="search-bar">
       This is the search bar.
     </div>
   )
